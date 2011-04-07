664808ab2d3ce72a45fef7bf3d082416d71b0ae4
veosotano <veosotano@gmail.com>
2011-04-07 19:32:47 +0200

    I'm not using SDL

--- SDL Test2/main.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  main.cpp
-//  SDL Test2
-//
-//  Created by Miro Keller on 03/04/11.
-//  Copyright 2011 Miro Keller. All rights reserved.
-//
-
-#include "main.h"
--- SDL Test2/main.h
+++
@@ -1,7 +0,0 @@
-//
-//  main.h
-//  SDL Test2
-//
-//  Created by Miro Keller on 03/04/11.
-//  Copyright 2011 Miro Keller. All rights reserved.
-//
