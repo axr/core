77b52b90f5f9c4563168b987e968627a809046f2
Ragnis Armus <ragnis.armus@gmail.com>
2012-06-17 21:23:19 +0300

    Return the correct resources path

--- prototype/core/os/linux/LinuxAxrWrapper.cpp
+++ prototype/core/os/linux/LinuxAxrWrapper.cpp
@@ -169,23 +169,23 @@ void LinuxAxrWrapper::setNeedsDisplay(bool newValue)
 std::string LinuxAxrWrapper::getPathToHSSFramework()
 {
 	// Code taken from: http://www.gamedev.net/community/forums/topic.asp?topic_id=459511
-    std::string path = "";
-    pid_t pid = getpid();
-    char buf[20] = {0};
-    sprintf(buf,"%d",pid);
-    std::string _link = "/proc/";
-    _link.append( buf );
-    _link.append( "/exe");
-    char proc[512];
-    int ch = readlink(_link.c_str(),proc,512);
-    if (ch != -1)
-    {
-        proc[ch] = 0;
-        path = proc;
-        std::string::size_type t = path.find_last_of("/");
-        path = path.substr(0,t);
-    }
+	std::string path = "";
+	pid_t pid = getpid();
+	char buf[20] = {0};
+	sprintf(buf,"%d",pid);
+	std::string _link = "/proc/";
+	_link.append( buf );
+	_link.append( "/exe");
+	char proc[512];
+	int ch = readlink(_link.c_str(),proc,512);
+	if (ch != -1)
+	{
+	proc[ch] = 0;
+	path = proc;
+	std::string::size_type t = path.find_last_of("/");
+	path = path.substr(0,t);
+	}
 
-    return path + std::string("/");
+	return path + std::string("/rsrc");
 }
 
