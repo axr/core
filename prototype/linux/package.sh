d3f4bd63e251eb635cd4f940014ace6f404e30ba
Ragnis Armus <ragnis.armus@gmail.com>
2012-06-19 13:40:10 +0300

    Take version as a CLI argument

--- prototype/linux/package.sh
+++ prototype/linux/package.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-VERSION=0.47
+version="$1"
+
+if [ -z $version ]; then
+	version=$(git rev-parse --verify HEAD)
+fi
+
+get_tutorial ()
+{
+	temp="$(mktemp -d)"
+	git clone git://github.com/AXR/Extras.git "$temp"
+
+	tutorial="$temp/Tutorials/000_tutorial_html/"
+}
 
 package ()
 {
@@ -10,19 +22,30 @@ package ()
 	cp ./axr_$1 ./package/
 	cp ../core/hss/framework/UIFramework.hss ./package/rsrc/
 	cp -r ../tests/ ./package/
-	#cp -r ./tutorial/ ./package/
+	cp -r "$tutorial" ./package/tutorial
 
 	cd ./package/
-	tar -czf axr_${VERSION}_linux_$2.tar.gz ./*
+	tar -czf ../axr_${version}_linux_$2.tar.gz ./*
 
-	rm -rf ./package/
+	cd ..
+	rm -rf ./package
 }
 
+echo "Packaging AXR protptype version $version"
+
+echo "Cloning the tutorial from Extras repo..."
+get_tutorial
+
+# Build resources directory
+./build_rsrc.sh
+
 if [ -f ./axr_64 ]; then
+	echo "x86-64 build found. Packaging..."
 	package "64" "x86-64"
 fi
 
 if [ -f ./axr_32 ]; then
+	echo "x86 build found. Packaging..."
 	package "32" "x86"
 fi
 
