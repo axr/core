506bf86d7da0a896686e639e4c0055607dab54b6
Ragnis Armus <ragnis.armus@gmail.com>
2012-08-22 23:00:09 +0300

    Dependency installation with pacman

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -10,7 +10,13 @@ if [ "`uname`" == "Darwin" ] ; then
     sudo port install cairo-devel +quartz +universal
     sudo port install pango-devel +quartz +universal
 elif [ "`uname`" == "Linux" ] ; then
-    echo "TODO: Install necessary Debian/RPM packages"
+    # For Arch Linux
+    which pacman > /dev/null 2>&1
+    if [ $? -eq 0 ] ; then
+        sudo pacman -S qt boost sdl expat cairo pango
+    else
+        echo "TODO: Install necessary Debian/RPM packages"
+    fi
 else
     echo "Unknown platform - you will have to find which packages to install manually, or build from source"
     echo "- Qt 4 (once the new prototype app is developed)"
