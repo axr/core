7f7848e9b4086e22f8c2755954e03a8239f31883
Jake Petroules <jake.petroules@petroules.com>
2012-08-22 22:23:25 -0400

    *Properly* make use of root in the dependency installer script.

--- dependencies/install.sh
+++ dependencies/install.sh
@@ -1,19 +1,24 @@
 #!/bin/sh
 
+if [ $EUID -ne 0 ] ; then
+    echo "This script must be run as root" 1>&2
+    exit 1
+fi
+
 # Installs dependencies for building AXR
 
 if [ "`uname`" == "Darwin" ] ; then
-    sudo port install qt4-mac +debug +framework +quartz +universal
-    sudo port install boost +universal
-    sudo port install libsdl-devel +universal
-    sudo port install expat +universal
-    sudo port install cairo-devel +quartz +universal
-    sudo port install pango-devel +quartz +universal
+    port install qt4-mac +debug +framework +quartz +universal
+    port install boost +universal
+    port install libsdl-devel +universal
+    port install expat +universal
+    port install cairo-devel +quartz +universal
+    port install pango-devel +quartz +universal
 elif [ "`uname`" == "Linux" ] ; then
     # For Arch Linux
     which pacman > /dev/null 2>&1
     if [ $? -eq 0 ] ; then
-        sudo pacman -S qt boost sdl expat cairo pango
+        pacman -S qt boost sdl expat cairo pango
     else
         echo "TODO: Install necessary Debian/RPM packages"
     fi
