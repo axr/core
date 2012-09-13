78914c031983370635985bd741b129e31c73c4b6
Jake Petroules <jake.petroules@petroules.com>
2012-09-12 23:02:26 -0400

    Prepend AXR_ to almost all of our CMake option() commands.
    This avoids potential conflicts with built-in variables and gives a nice
    overview of the available options when using the GUI as they will be
    listed alphabetically.
    
    BUILD_UNIVERSAL_BINS is close enough in name and function class to the
    built-in BUILD_SHARED_LIBS, so it can stay as it is.

--- .travis/configure.sh
+++ .travis/configure.sh
@@ -9,8 +9,17 @@ mkdir ../build-shared ../build-static
 
 # Configure using CMake (shared)
 cd ../build-shared
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=ON -DALL_WARNINGS=ON ../Prototype
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DAXR_ALL_WARNINGS=ON \
+    -DAXR_BUILD_EXAMPLES=ON \
+    -DBUILD_SHARED_LIBS=ON \
+    ../Prototype
 
 # Configure using CMake (static)
 cd ../build-static
-cmake -DCMAKE_BUILD_TYPE=Release -DALL_WARNINGS=ON ../Prototype
+cmake \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DAXR_ALL_WARNINGS=ON \
+    -DAXR_BUILD_EXAMPLES=ON \
+    ../Prototype
