b5e5e3dd14d9e815ba917ff835140afef2f3a3ce
Jake Petroules <jake.petroules@petroules.com>
2012-09-13 01:52:13 -0400

    Add function to detect the Clang compiler.
    We need to set -Weverything for Clang only; GCC doesn't support it.

--- cmake/Utilities.cmake
+++ cmake/Utilities.cmake
@@ -1,3 +1,34 @@
+# Detects whether a preprocessor symbol is defined by the current C compiler
+function(is_symbol_defined output_variable symbol)
+    enable_language(C)
+
+set(is_symbol_defined_code "
+#if defined(${symbol})
+int main() { return 0; }
+#endif
+")
+
+    file(WRITE "${CMAKE_BINARY_DIR}/is_symbol_defined.c" "${is_symbol_defined_code}")
+
+    try_compile(is_symbol_defined_result "${CMAKE_BINARY_DIR}" "${CMAKE_BINARY_DIR}/is_symbol_defined.c")
+
+    if(is_symbol_defined_result)
+        set(${output_variable} TRUE PARENT_SCOPE)
+    else()
+        set(${output_variable} FALSE PARENT_SCOPE)
+    endif()
+endfunction()
+
+# Checks whether the compiler in use is Clang by asking the compiler itself
+# CMAKE_COMPILER_IS_CLANGXX will be set in the parent scope if this is the case
+function(check_clang)
+    is_symbol_defined(clang_result __clang__)
+
+    if(clang_result)
+        set(CMAKE_COMPILER_IS_CLANGXX TRUE PARENT_SCOPE)
+    endif()
+endfunction()
+
 # NOTE: Untested on Windows
 function(check_pathnames)
     if(WIN32)
