71159f4a4a04a859b5fdf981fdf245b7b0211713
Jake Petroules <jake.petroules@petroules.com>
2012-09-17 03:52:10 -0400

    CPack WIP. OS X packages work, aside from needing some polishing.

---
+++ cmake/Package.cmake
@@ -0,0 +1,109 @@
+# for NSIS to create Start Menu shortcuts
+#set(CPACK_PACKAGE_EXECUTABLES "${TARGET_NAME};${CMAKE_PROJECT_NAME}")
+
+# Per-generator overrides
+set(CPACK_PROJECT_CONFIG_FILE "${CMAKE_BINARY_DIR}/PackageOverrides.cmake")
+
+# PackageMaker only allows .rtf, .rtfd, .html and .txt
+configure_file("${CMAKE_SOURCE_DIR}/cmake/PackageOverrides.cmake.in" "${CPACK_PROJECT_CONFIG_FILE}")
+configure_file("${AXR_README_FILE}" "${AXR_README_FILE_TXT}")
+configure_file("${AXR_LICENSE_FILE}" "${AXR_LICENSE_FILE_TXT}")
+
+# Package version
+set(CPACK_PACKAGE_VERSION_MAJOR ${AXR_VERSION_MAJOR})
+set(CPACK_PACKAGE_VERSION_MINOR ${AXR_VERSION_MINOR})
+set(CPACK_PACKAGE_VERSION_PATCH ${AXR_VERSION_PATCH})
+set(CPACK_PACKAGE_VERSION ${CPACK_PACKAGE_VERSION_MAJOR}.${CPACK_PACKAGE_VERSION_MINOR}.${CPACK_PACKAGE_VERSION_PATCH}.${CPACK_PACKAGE_VERSION_BUILD})
+
+# CPack variables...
+set(CPACK_PACKAGE_ICON "${AXR_PACKAGE_ICON}")
+set(CPACK_PACKAGE_NAME "${AXR_PACKAGE_NAME}")
+set(CPACK_PACKAGE_DESCRIPTION "")
+set(CPACK_PACKAGE_DESCRIPTION_SUMMARY "")
+set(CPACK_PACKAGE_VENDOR "${AXR_VENDOR}")
+set(CPACK_PACKAGE_CONTACT "${AXR_CONTACT}")
+set(CPACK_PACKAGE_DESCRIPTION_FILE "${AXR_README_FILE_TXT}")
+set(CPACK_RESOURCE_FILE_README "${AXR_README_FILE_TXT}")
+set(CPACK_RESOURCE_FILE_LICENSE "${AXR_LICENSE_FILE_TXT}")
+
+# Installation types...
+set(CPACK_ALL_INSTALL_TYPES Full Developer)
+
+# Friendly names, descriptions and groups for components
+set(CPACK_COMPONENT_PROTOTYPE_DISPLAY_NAME "Prototype Application")
+set(CPACK_COMPONENT_PROTOTYPE_DESCRIPTION "An pseudo-browser that can be used to test the AXR rendering engine and HSS language")
+set(CPACK_COMPONENT_PROTOTYPE_GROUP "Runtime")
+set(CPACK_COMPONENT_PROTOTYPE_INSTALL_TYPES Full Developer)
+set(CPACK_COMPONENT_PROTOTYPE_REQUIRED TRUE)
+
+set(CPACK_COMPONENT_LIBRARIES_GROUP "Runtime")
+set(CPACK_COMPONENT_LIBRARIES_REQUIRED TRUE)
+set(CPACK_COMPONENT_LIBRARIES_INSTALL_TYPES Full Developer)
+set(CPACK_COMPONENT_LIBRARIES_HIDDEN TRUE)
+
+set(CPACK_COMPONENT_FRAMEWORKS_GROUP "Runtime")
+set(CPACK_COMPONENT_FRAMEWORKS_REQUIRED TRUE)
+set(CPACK_COMPONENT_FRAMEWORKS_INSTALL_TYPES Full Developer)
+set(CPACK_COMPONENT_FRAMEWORKS_HIDDEN TRUE)
+
+set(CPACK_COMPONENT_EXAMPLES_DISPLAY_NAME "Example Applications")
+set(CPACK_COMPONENT_EXAMPLES_DESCRIPTION "Example programs demonstrating how the AXR core library can be used")
+set(CPACK_COMPONENT_EXAMPLES_GROUP "Development")
+set(CPACK_COMPONENT_EXAMPLES_INSTALL_TYPES Developer)
+
+set(CPACK_COMPONENT_DOC_DISPLAY_NAME "Documentation")
+set(CPACK_COMPONENT_DOC_DESCRIPTION "Documentation for the AXR SDK and examples")
+set(CPACK_COMPONENT_DOC_GROUP "Development")
+set(CPACK_COMPONENT_DOC_INSTALL_TYPES Developer)
+
+# Describe component groups
+set(CPACK_COMPONENT_GROUP_RUNTIME_DESCRIPTION "")
+set(CPACK_COMPONENT_GROUP_DEVELOPMENT_DESCRIPTION "All of the tools required to develop applications using AXR.\n\nNote that the Qt SDK and Boost C++ libraries must be installed separately, as well as a compiler environment (such as Visual Studio or Xcode).")
+
+# Platform name
+string(TOLOWER "${CMAKE_SYSTEM_NAME}" OS_CODE)
+if(APPLE)
+    set(OS_CODE "osx")
+endif()
+
+# Filenames
+set(CPACK_PACKAGE_FILE_NAME "${AXR_PACKAGE_PREFIX}-${AXR_VERSION_STRING}-${OS_CODE}-${CMAKE_TARGET_ARCHITECTURE_CODE}")
+set(CPACK_SOURCE_PACKAGE_FILE_NAME "${AXR_PACKAGE_PREFIX}-${AXR_VERSION_STRING}-src")
+set(CPACK_SOURCE_IGNORE_FILES ".git" ".DS_Store" "thumbs.db" "CMakeLists.txt.user")
+
+# Source package
+list(APPEND CPACK_SOURCE_GENERATOR "ZIP")
+if(UNIX)
+    list(APPEND CPACK_SOURCE_GENERATOR TGZ STGZ TBZ2 TZ)
+endif()
+
+if(WIN32)
+    set(CPACK_GENERATOR "NSIS")
+    set(CPACK_NSIS_MUI_ICON "${AXR_PACKAGE_ICON}")
+    set(CPACK_NSIS_MUI_UNIICON "${AXR_PACKAGE_ICON}")
+#    set(CPACK_PACKAGE_ICON "<something>.bmp")
+    set(CPACK_NSIS_INSTALLED_ICON_NAME "bin\\\\prototype.exe")
+    set(CPACK_NSIS_HELP_LINK "http:\\\\\\\\axr.vg")
+    set(CPACK_NSIS_URL_INFO_ABOUT "http:\\\\\\\\axr.vg")
+    set(CPACK_NSIS_CONTACT "${AXR_CONTACT}")
+endif()
+
+if(APPLE)
+    set(CPACK_GENERATOR "DragNDrop;PackageMaker")
+    set(CPACK_DMG_VOLUME_NAME "${AXR_PACKAGE_NAME} ${AXR_VERSION_STRING}")
+    set(CPACK_OSX_PACKAGE_VERSION ${CMAKE_OSX_DEPLOYMENT_TARGET})
+endif()
+
+if(__DEB)
+    set(CPACK_GENERATOR "DEB")
+    set(CPACK_DEBIAN_PACKAGE_MAINTAINER "${AXR_VENDOR} <${AXR_CONTACT}>")
+    set(CPACK_DEBIAN_PACKAGE_DEPENDS "qt-sdk (>= 4.8), libboost") # TODO: probably the wrong Boost package
+    set(CPACK_DEBIAN_PACKAGE_SECTION "Development")
+    set(CPACK_DEBIAN_PACKAGE_VERSION "${CPACK_PACKAGE_VERSION}+lenny1")
+endif()
+
+if(__RPM)
+    set(CPACK_GENERATOR "RPM")
+    set(CPACK_RPM_PACKAGE_REQUIRES "qt >= 4.8")
+    set(CPACK_RPM_PACKAGE_PROVIDES "axr-sdk")
+endif()
--- cmake/Utilities.cmake
+++ cmake/Utilities.cmake
@@ -1,3 +1,5 @@
+include(CMakeParseArguments)
+
 # NOTE: Untested on Windows
 function(check_pathnames)
     if(WIN32)
@@ -9,4 +11,72 @@ function(check_pathnames)
     if(NOT "${CMAKE_SOURCE_DIR}" MATCHES "${path_regex}" OR NOT "${CMAKE_BINARY_DIR}" MATCHES "${path_regex}")
         message(FATAL_ERROR "Ensure that the source and build paths contain only the following characters: alphanumeric, dash, underscore, slash, dot (and colon on Windows)")
     endif()
+endfunction()
+
+function(join_path output_variable components)
+    set(final_path .)
+    foreach(component ${components})
+        set(final_path "${final_path}/${component}")
+    endforeach()
+
+    while("${final_path}" MATCHES "^\\./")
+        string(SUBSTRING "${final_path}" 2 -1 final_path)
+    endwhile()
+
+    set(${output_variable} "${final_path}" PARENT_SCOPE)
+endfunction()
+
+function(install_bundle)
+    set(options )
+    set(oneValueArgs TARGET COMPONENT BUNDLE_DESTINATION RUNTIME_DESTINATION)
+    set(multiValueArgs )
+    cmake_parse_arguments(install_bundle "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN} )
+
+    if(NOT install_bundle_TARGET)
+        message(FATAL_ERROR "install_bundle missing TARGET argument")
+    endif()
+
+    if(NOT install_bundle_COMPONENT)
+        message(FATAL_ERROR "install_bundle missing COMPONENT argument")
+    endif()
+
+    if(NOT install_bundle_BUNDLE_DESTINATION)
+        message(FATAL_ERROR "install_bundle missing BUNDLE_DESTINATION argument")
+    endif()
+
+    if(NOT install_bundle_RUNTIME_DESTINATION)
+        message(FATAL_ERROR "install_bundle missing RUNTIME_DESTINATION argument")
+    endif()
+
+    if(APPLE)
+        join_path(path_bundle     "${install_bundle_BUNDLE_DESTINATION};${install_bundle_TARGET}.app")
+        join_path(path_plugin_dir "${install_bundle_BUNDLE_DESTINATION};${install_bundle_TARGET}.app/Contents/MacOS")
+        join_path(path_qtconf_dir "${install_bundle_BUNDLE_DESTINATION};${install_bundle_TARGET}.app/Contents/MacOS")
+    else()
+        join_path(path_bundle     "${install_bundle_RUNTIME_DESTINATION};${install_bundle_TARGET}${CMAKE_EXECUTABLE_SUFFIX}")
+        join_path(path_plugin_dir "${install_bundle_RUNTIME_DESTINATION}")
+        join_path(path_qtconf_dir "${install_bundle_RUNTIME_DESTINATION}")
+    endif()
+
+    install(TARGETS ${install_bundle_TARGET}
+        BUNDLE DESTINATION ${install_bundle_BUNDLE_DESTINATION} COMPONENT ${install_bundle_COMPONENT}
+        RUNTIME DESTINATION ${install_bundle_RUNTIME_DESTINATION} COMPONENT ${install_bundle_COMPONENT}
+    )
+
+    # Install Qt plugins - can be culled using 'REGEX "..." EXCLUDE'
+    # TODO: Install plugins according to which Qt libraries are linked to the target
+    install(DIRECTORY "${QT_PLUGINS_DIR}/imageformats" DESTINATION "${path_plugin_dir}/plugins" COMPONENT ${install_bundle_COMPONENT})
+
+    list(APPEND LIBDIRS "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}")
+    list(APPEND LIBDIRS "${CMAKE_LIBRARY_OUTPUT_DIRECTORY}/${CMAKE_BUILD_TYPE}")
+
+    # Make the application bundle standalone
+    file(WRITE "${CMAKE_BINARY_DIR}/${install_bundle_TARGET}.Target.CPack.txt" "
+        file(WRITE \"\${CMAKE_INSTALL_PREFIX}/${path_qtconf_dir}/qt.conf\" \"\")
+        file(GLOB_RECURSE QTPLUGINS \"\${CMAKE_INSTALL_PREFIX}/${path_plugin_dir}/plugins/*${CMAKE_SHARED_LIBRARY_SUFFIX}\")
+        include(BundleUtilities)
+        fixup_bundle(\"\${CMAKE_INSTALL_PREFIX}/${path_bundle}\" \"\${QTPLUGINS}\" \"${LIBDIRS}\")
+        ")
+
+    install(SCRIPT "${CMAKE_BINARY_DIR}/${install_bundle_TARGET}.Target.CPack.txt" COMPONENT ${install_bundle_COMPONENT})
 endfunction()
