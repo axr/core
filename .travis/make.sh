d00020569b97f8661311b25c740e0ff568c829b8
Jake Petroules <jake.petroules@petroules.com>
2012-09-20 22:52:47 -0400

    Build distribution packages in CI (just archives now, not deb/rpm yet).

--- .travis/make.sh
+++ .travis/make.sh
@@ -3,5 +3,5 @@ set -e
 
 # Build all variants
 cd ..
-make -C build-shared all docs package_source #package
-make -C build-static all docs package_source #package
+make -C build-shared all package package_source
+make -C build-static all package package_source
