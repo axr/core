e75e0f757b8c3fb4c9a819fc01b3d3a8c7e162b2
Jake Petroules <jake.petroules@petroules.com>
2012-09-26 22:49:19 -0400

    CMake cleanup.
    The project() command was executed too late and may have resulted in
    incorrect package names.

--- cmake/Utilities.cmake
+++ cmake/Utilities.cmake
@@ -1,18 +1,5 @@
 include(CMakeParseArguments)
 
-# NOTE: Untested on Windows
-function(check_pathnames)
-    if(WIN32)
-        set(separator ":")
-    endif()
-
-    set(path_regex "^([A-Za-z0-9._/-${separator}]+)$")
-
-    if(NOT "${CMAKE_SOURCE_DIR}" MATCHES "${path_regex}" OR NOT "${CMAKE_BINARY_DIR}" MATCHES "${path_regex}")
-        message(FATAL_ERROR "Ensure that the source and build paths contain only the following characters: alphanumeric, dash, underscore, slash, dot (and colon on Windows)")
-    endif()
-endfunction()
-
 function(join_path output_variable components)
     set(final_path .)
     foreach(component ${components})
