82dcca9ad6d8a06efd5b13d551aba68be7df2168
Jake Petroules <jake.petroules@petroules.com>
2012-10-17 18:35:44 -0400

    Clean and document AXRError.

--- src/core/debug/AXRError.cpp
+++ src/core/debug/AXRError.cpp
@@ -51,25 +51,43 @@ using namespace AXR;
 class AXRError::Data : public QSharedData
 {
 public:
-    Data(const AXRString &origin, const AXRString &message, const QUrl &url, int line, int column, bool inFile)
-    : origin(origin), message(message), url(url), line(line),
-    column(column), inFile(inFile) { }
+    Data(const AXRString &origin, const AXRString &message, const QUrl &url, int line, int column)
+    : origin(origin), message(message), url(url), line(line), column(column) { }
+
     Data(const Data &other)
     : QSharedData(other), origin(other.origin), message(other.message),
-    url(other.url), line(other.line), column(other.column),
-    inFile(other.inFile) { }
+    url(other.url), line(other.line), column(other.column) { }
+
     ~Data() { }
 
+    /**
+     * The origin of the error message, typically the class from which it was triggered.
+     */
     AXRString origin;
+
+    /**
+     * A human-readable error message describing the problem.
+     */
     AXRString message;
+
+    /**
+     * The URL of the XML or HSS file in which the problem occurred.
+     */
     QUrl url;
+
+    /**
+     * The line in the XML or HSS file on which the problem occurred.
+     */
     int line;
+
+    /**
+     * The column in the XML or HSS file on which the problem occurred.
+     */
     int column;
-    bool inFile;
 };
 
 AXRError::AXRError(const AXRString &origin, const AXRString &message, const QUrl &url, int line, int column)
-: d(new Data(origin, message, url, line, column, !url.isEmpty()))
+: d(new Data(origin, message, url, line, column))
 {
 }
 
@@ -97,7 +115,8 @@ AXRString AXRError::toProblemString(const AXRString &label) const
     QStringList parts;
     parts << AXRString("%1: %2").arg(label).arg(d->message);
 
-    if (d->inFile)
+    // If we have a URL the source is a file, otherwise it's from a memory buffer
+    if (!d->url.isEmpty())
         parts << AXRString("in %1").arg(d->url.toString());
 
     if (d->line > 0 || d->column > 0)
