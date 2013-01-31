f59eb78732e6aa8a2a157314af4305dd9291a8a7
Jake Petroules <jake.petroules@petroules.com>
2013-01-31 01:47:09 -0500

    Add classes to begin the process of abstracting input events.

---
+++ src/core/hss/input/HSSMouseEvent.cpp
@@ -0,0 +1,85 @@
+/********************************************************************
+ *             a  A
+ *            AM\/MA
+ *           (MA:MMD
+ *            :: VD
+ *           ::  º
+ *          ::
+ *         ::   **      .A$MMMMND   AMMMD     AMMM6    MMMM  MMMM6
+ +       6::Z. TMMM    MMMMMMMMMDA   VMMMD   AMMM6     MMMMMMMMM6
+ *      6M:AMMJMMOD     V     MMMA    VMMMD AMMM6      MMMMMMM6
+ *      ::  TMMTMC         ___MMMM     VMMMMMMM6       MMMM
+ *     MMM  TMMMTTM,     AMMMMMMMM      VMMMMM6        MMMM
+ *    :: MM TMMTMMMD    MMMMMMMMMM       MMMMMM        MMMM
+ *   ::   MMMTTMMM6    MMMMMMMMMMM      AMMMMMMD       MMMM
+ *  :.     MMMMMM6    MMMM    MMMM     AMMMMMMMMD      MMMM
+ *         TTMMT      MMMM    MMMM    AMMM6  MMMMD     MMMM
+ *        TMMMM8       MMMMMMMMMMM   AMMM6    MMMMD    MMMM
+ *       TMMMMMM$       MMMM6 MMMM  AMMM6      MMMMD   MMMM
+ *      TMMM MMMM
+ *     TMMM  .MMM
+ *     TMM   .MMD       ARBITRARY·······XML········RENDERING
+ *     TMM    MMA       ====================================
+ *     TMN    MM
+ *      MN    ZM
+ *            MM,
+ *
+ *
+ *      AUTHORS: see AUTHORS file
+ *
+ *      COPYRIGHT: ©2012 - All Rights Reserved
+ *
+ *      LICENSE: see LICENSE file
+ *
+ *      WEB: http://axr.vg
+ *
+ *      THIS CODE AND INFORMATION ARE PROVIDED "AS IS"
+ *      WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED
+ *      OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *      IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR
+ *      FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ ********************************************************************/
+
+#include "HSSMouseEvent.h"
+#include "HSSPoint.h"
+
+using namespace AXR;
+
+class HSSMouseEvent::Private
+{
+public:
+    Private() : type(), position()
+    {
+    }
+
+    HSSEventType type;
+    HSSPoint position;
+};
+
+HSSMouseEvent::HSSMouseEvent(HSSEventType type, const HSSPoint &position)
+: d(new Private)
+{
+    d->type = type;
+    d->position = position;
+}
+
+HSSMouseEvent::~HSSMouseEvent()
+{
+    delete d;
+}
+
+const HSSPoint& HSSMouseEvent::position() const
+{
+    return d->position;
+}
+
+HSSUnit HSSMouseEvent::x() const
+{
+    return d->position.x;
+}
+
+HSSUnit HSSMouseEvent::y() const
+{
+    return d->position.y;
+}
