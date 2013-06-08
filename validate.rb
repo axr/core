2cca57e4249e4f0dcee58880e9f4bf593576c8cf
Ragnis Armus <ragnis.armus@gmail.com>
2013-06-08 20:21:34 +0300

    Add validator

---
+++ validate.rb
@@ -0,0 +1,17 @@
+require_relative "../common/validator/validator.rb"
+
+validator = AXRValidator::Validator.new
+scanner = AXRValidator::FileScanner.new
+
+# Default is :fs
+scanner.source = :cached if ARGV.include? "--cached"
+
+scanner.on(/.*\.(h|c|cpp|m|mm)$/) do |file|
+  generic = AXRValidator::Validators::Generic.new validator, file
+  generic.validate_encoding_utf8
+  generic.validate_whitespace
+  generic.validate_indentation " "
+end
+
+validator.messages.each { |msg| puts "#{msg.to_string}\n\n" }
+validator.end
