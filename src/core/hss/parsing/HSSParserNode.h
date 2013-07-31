ecf85dafbfcf1c4a2486ab30856f1250d4cba003
veosotano <veosotano@gmail.com>
2013-07-31 19:52:53 +0100

    Reorder functions so that they are the same as in the implementation

--- src/core/hss/parsing/HSSParserNode.h
+++ src/core/hss/parsing/HSSParserNode.h
@@ -205,18 +205,18 @@ namespace AXR
 
         QSharedPointer<HSSParserNode> shared_from_this();
 
+       /**
+        *  Getter for the controller.
+        *  @return A pointer to the AXRController that owns this object
+        */
+       virtual AXRController* getController();
+
         /**
          *  Setter for the controller. The controller needs to be propagated across all
          *  HSSParserNode subclasses, so they get access to the DOM and such.
          *  @param controller       A pointer to the AXRController that owns this object
          */
         virtual void setController(AXRController *controller);
-
-        /**
-         *  Getter for the controller.
-         *  @return A pointer to the AXRController that owns this object
-         */
-        virtual AXRController* getController();
 
        /**
         *  Getter for the host property.
