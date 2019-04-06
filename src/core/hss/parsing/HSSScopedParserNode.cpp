fd53f403bc37c4f1689db07bd75173456d625726
veosotano <veosotano@gmail.com>
2019-04-06 12:26:04 +0100

    Add HSSScopedParserNode as a superclass for all parser nodes that handle scope

---
+++ src/core/hss/parsing/HSSScopedParserNode.cpp
@@ -0,0 +1,69 @@
+/********************************************************************
+ *             a  A
+ *            AM\/MA
+ *           (MA:MMD
+ *            :: VD
+ *           ::  º
+ *          ::
+ *         ::   **      .A$MMMMND   AMMMD     AMMM6    MMMM  MMMM6
+ +       6::Z. TMMM    MMMMMMMMMDA   VMMMD   AMMM6     MMMMMMMMM6
+ *      6M:AMMJMMOD     V     MMMA    VMMMD AMMM6      MMMMMMM6
+ *      ::  TMMTMC         ___MMMM     VMMMMMMM6       MMMM
+ *     MMM  TMMMTTM,     AMMMMMMMM      VMMMMM6        MMMM
+ *    :: MM TMMTMMMD    MMMMMMMMMM       MMMMMM        MMMM
+ *   ::   MMMTTMMM6    MMMMMMMMMMM      AMMMMMMD       MMMM
+ *  :.     MMMMMM6    MMMM    MMMM     AMMMMMMMMD      MMMM
+ *         TTMMT      MMMM    MMMM    AMMM6  MMMMD     MMMM
+ *        TMMMM8       MMMMMMMMMMM   AMMM6    MMMMD    MMMM
+ *       TMMMMMM$       MMMM6 MMMM  AMMM6      MMMMD   MMMM
+ *      TMMM MMMM
+ *     TMMM  .MMM
+ *     TMM   .MMD       ARBITRARY·······XML········RENDERING
+ *     TMM    MMA       ====================================
+ *     TMN    MM
+ *      MN    ZM
+ *            MM,
+ *
+ *
+ *      AUTHORS: see AUTHORS file
+ *
+ *      COPYRIGHT: ©2019 - All Rights Reserved
+ *
+ *      LICENSE: see LICENSE file
+ *
+ *      WEB: http://axrproject.org
+ *
+ *      THIS CODE AND INFORMATION ARE PROVIDED "AS IS"
+ *      WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED
+ *      OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *      IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR
+ *      FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ ********************************************************************/
+
+#include "axr.h"
+#include "HSSScopedParserNode.h"
+
+using namespace AXR;
+
+HSSScopedParserNode::HSSScopedParserNode(HSSParserNodeType type, AXRController * controller)
+: HSSParserNode(type, controller)
+{
+    
+}
+
+HSSScopedParserNode::HSSScopedParserNode(const HSSScopedParserNode & other)
+: HSSParserNode(other)
+{
+    this->_scope = other._scope;
+}
+
+void HSSScopedParserNode::setScope(QSharedPointer<HSSSimpleSelection> newScope)
+{
+    this->_scope = newScope;
+}
+
+QSharedPointer<HSSSimpleSelection> HSSScopedParserNode::scope()
+{
+    return this->_scope;
+}
---
+++ src/core/hss/parsing/HSSScopedParserNode.h
@@ -0,0 +1,72 @@
+/********************************************************************
+ *             a  A
+ *            AM\/MA
+ *           (MA:MMD
+ *            :: VD
+ *           ::  º
+ *          ::
+ *         ::   **      .A$MMMMND   AMMMD     AMMM6    MMMM  MMMM6
+ +       6::Z. TMMM    MMMMMMMMMDA   VMMMD   AMMM6     MMMMMMMMM6
+ *      6M:AMMJMMOD     V     MMMA    VMMMD AMMM6      MMMMMMM6
+ *      ::  TMMTMC         ___MMMM     VMMMMMMM6       MMMM
+ *     MMM  TMMMTTM,     AMMMMMMMM      VMMMMM6        MMMM
+ *    :: MM TMMTMMMD    MMMMMMMMMM       MMMMMM        MMMM
+ *   ::   MMMTTMMM6    MMMMMMMMMMM      AMMMMMMD       MMMM
+ *  :.     MMMMMM6    MMMM    MMMM     AMMMMMMMMD      MMMM
+ *         TTMMT      MMMM    MMMM    AMMM6  MMMMD     MMMM
+ *        TMMMM8       MMMMMMMMMMM   AMMM6    MMMMD    MMMM
+ *       TMMMMMM$       MMMM6 MMMM  AMMM6      MMMMD   MMMM
+ *      TMMM MMMM
+ *     TMMM  .MMM
+ *     TMM   .MMD       ARBITRARY·······XML········RENDERING
+ *     TMM    MMA       ====================================
+ *     TMN    MM
+ *      MN    ZM
+ *            MM,
+ *
+ *
+ *      AUTHORS: see AUTHORS file
+ *
+ *      COPYRIGHT: ©2019 - All Rights Reserved
+ *
+ *      LICENSE: see LICENSE file
+ *
+ *      WEB: http://axrproject.org
+ *
+ *      THIS CODE AND INFORMATION ARE PROVIDED "AS IS"
+ *      WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED
+ *      OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ *      IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR
+ *      FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ ********************************************************************/
+
+#ifndef HSSSCOPEDPARSERNODE_H
+#define HSSSCOPEDPARSERNODE_H
+
+#include "HSSParserNode.h"
+
+namespace AXR
+{
+    class HSSSimpleSelection;
+
+    class AXR_API HSSScopedParserNode : public HSSParserNode
+    {
+    public:
+        HSSScopedParserNode(HSSParserNodeType type, AXRController * controller);
+        HSSScopedParserNode(const HSSScopedParserNode & other);
+
+        /**
+         *  Setter for the scope which to pass to members like references or selections.
+         *  @param newScope     The new scope, a shared pointer to a simple selection.
+         */
+        virtual void setScope(QSharedPointer<HSSSimpleSelection> newScope);
+        
+        QSharedPointer<HSSSimpleSelection> scope();
+        
+    private:
+        QSharedPointer<HSSSimpleSelection> _scope;
+    };
+}
+
+#endif
